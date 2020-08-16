216582a78153980c40d0f999793c86400b576b68
Gareth Reid <gareth.reid@petermac.org>
2020-08-16 10:35:58 +1000

    switched to syncfusion FW

---
+++ EssentialUIKit.UWP/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Syncfusion.ListView.XForms.UWP;
+
+namespace EssentialUIKit.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SfListViewRenderer.Init();
+            new Syncfusion.SfMaps.XForms.UWP.SfMapsRenderer();
+            LoadApplication(new EssentialUIKit.App());
+        }
+    }
+}
